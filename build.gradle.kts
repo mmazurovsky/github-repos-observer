7023ddb2ce49c6df7be5a8ed3ccb1818f6e7d45c
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2025-05-12 16:14:47 +0200

    - init project

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.5"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.mmazurovsky"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // JSON processing
+    implementation("com.fasterxml.jackson.module:jackson-module-jakarta-xmlbind-annotations")
+    // Retries logic
+    implementation("org.springframework.retry:spring-retry")
+    // Logging
+    implementation("org.slf4j:slf4j-api")
+    // Testing
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/test/java/com/mmazurovsky/redcarecase/RedcareCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmazurovsky.redcarecase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedcareCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
