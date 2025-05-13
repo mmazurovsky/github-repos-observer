048dce16e73884b18ed6fb6058586ad155689a82
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2025-05-13 12:02:35 +0200

    - made some of fields in RepositoriesSearchIn optional - modified validation annotations

--- src/main/java/com/mmazurovsky/redcarecase/dto/in/RepositoriesSearchIn.java
+++ src/main/java/com/mmazurovsky/redcarecase/dto/in/RepositoriesSearchIn.java
@@ -1,23 +1,26 @@
 package com.mmazurovsky.redcarecase.dto.in;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 public record RepositoriesSearchIn(
-        @Size(max = 50, message = "Search keywords must be 50 characters or less")
+        @Size(max = 50, min = 1, message = "Search keywords must be 50 characters or less")
         String keywords,
 
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-        @Past(message = "Earliest created date must be in the past")
-        LocalDate earliestCreatedDate,
+        Optional<@Past(message = "Earliest created date must be in the past") LocalDate> earliestCreatedDate,
 
-        @Pattern(
+        Optional<@Pattern(
                 regexp = "^[a-zA-Z0-9]+$",
                 message = "Programming language must be a single string without spaces or commas"
-        )
-        String language
-) {}
+        ) String> language,
+
+        Optional<@Max(value = 20, message = "Max pages to be searched must be less than or equal to 20") Integer> maxPages
+) {
+}
