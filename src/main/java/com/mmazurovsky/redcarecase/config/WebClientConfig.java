3185a7adbce2b70e49449d14c1c6f610876f04bb
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2025-05-16 11:23:00 +0200

    - adjusted client logging

--- src/main/java/com/mmazurovsky/redcarecase/config/WebClientConfig.java
+++ src/main/java/com/mmazurovsky/redcarecase/config/WebClientConfig.java
@@ -1,27 +1,23 @@
 package com.mmazurovsky.redcarecase.config;
 
-import com.mmazurovsky.redcarecase.service.SearchServiceImpl;
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 import reactor.netty.resources.ConnectionProvider;
 import reactor.netty.http.client.HttpClient;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
+import static com.mmazurovsky.redcarecase.util.ClientLoggingUtil.logRequest;
+
 @Configuration
 public class WebClientConfig {
-    private static final Logger logger = LoggerFactory.getLogger(WebClientConfig.class);
 
     @Value("${github.api.token}")
     private String githubToken;
@@ -35,7 +31,7 @@ public class WebClientConfig {
                 .build();
 
         HttpClient httpClient = HttpClient.create(provider)
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 20000) // 10 seconds
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 20000)
                 .responseTimeout(Duration.ofSeconds(20))
                 .doOnConnected(conn -> conn
                         .addHandlerLast(new ReadTimeoutHandler(20, TimeUnit.SECONDS))
@@ -53,26 +49,5 @@ public class WebClientConfig {
                 .defaultHeader("Authorization", "Bearer " + githubToken)
                 .filter(logRequest())
                 .build();
-    }
-
-    private ExchangeFilterFunction logRequest() {
-        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
-            StringBuilder log = new StringBuilder();
-            log.append("→ ")
-                    .append(clientRequest.method())
-                    .append(" ")
-                    .append(clientRequest.url());
-
-            clientRequest.headers().forEach((name, values) -> {
-                if (!name.equalsIgnoreCase("Authorization")) {
-                    log.append("\n  ").append(name).append(": ").append(String.join(",", values));
-                } else {
-                    log.append("\n  ").append(name).append(": [REDACTED]");
-                }
-            });
-
-            logger.debug(log.toString());
-            return Mono.just(clientRequest);
-        });
     }
 }
---
+++ src/main/java/com/mmazurovsky/redcarecase/util/ClientLoggingUtil.java
@@ -0,0 +1,32 @@
+package com.mmazurovsky.redcarecase.util;
+
+import com.mmazurovsky.redcarecase.config.WebClientConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import reactor.core.publisher.Mono;
+
+public class ClientLoggingUtil {
+    private static final Logger logger = LoggerFactory.getLogger(ClientLoggingUtil.class);
+
+    public static ExchangeFilterFunction logRequest() {
+        return ExchangeFilterFunction.ofRequestProcessor(clientRequest -> {
+            StringBuilder log = new StringBuilder();
+            log.append("→ ")
+                    .append(clientRequest.method())
+                    .append(" ")
+                    .append(clientRequest.url());
+
+            clientRequest.headers().forEach((name, values) -> {
+                if (!name.equalsIgnoreCase("Authorization")) {
+                    log.append("\n  ").append(name).append(": ").append(String.join(",", values));
+                } else {
+                    log.append("\n  ").append(name).append(": [REDACTED]");
+                }
+            });
+
+            logger.debug(log.toString());
+            return Mono.just(clientRequest);
+        });
+    }
+}
