7b1c7e4b5b9456770a8c03a7eefd6f1dfbe396bd
Mikhail Mazurovskiy <mmazurovsky@gmail.com>
2025-05-16 11:01:37 +0200

    - minor change in GlobalErrorWebExceptionHandler

--- src/main/java/com/mmazurovsky/redcarecase/errorhandling/GlobalErrorWebExceptionHandler.java
+++ src/main/java/com/mmazurovsky/redcarecase/errorhandling/GlobalErrorWebExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebExchangeBindException;
 import org.springframework.web.server.ResponseStatusException;
@@ -33,7 +34,7 @@ public class GlobalErrorWebExceptionHandler implements ErrorWebExceptionHandler 
     }
 
     @Override
-    public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+    public @NonNull Mono<Void> handle(ServerWebExchange exchange, @NonNull Throwable ex) {
         HttpStatusCode status = determineStatus(ex);
         exchange.getResponse().setStatusCode(status);
         exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
